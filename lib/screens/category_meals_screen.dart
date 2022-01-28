ce9c77b50be90d4e05f3f5eed110bf8c0e01410c
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-01-28 19:43:22 +0200

    Update 2: show on screen values of new data

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/categories-meals';
+  const CategoryMealsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeal = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                title: categoryMeal[index].title,
+                imgUrl: categoryMeal[index].imageUrl,
+                duration: categoryMeal[index].duration,
+                complexity: categoryMeal[index].complexity,
+                affordability: categoryMeal[index].affordability);
+          },
+          itemCount: categoryMeal.length,
+        ));
+  }
+}
