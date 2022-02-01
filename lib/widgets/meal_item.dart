1f967dea1a35b1ef7812f1fe54c5f73765e230bc
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-02-01 20:01:42 +0200

    Update 2: show on screen values of new data

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,37 +9,123 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
-  const MealItem({required this.title, required this.imgUrl, required this.duration, required this.complexity, required this.affordability, Key? key}) : super(key: key);
+  const MealItem(
+      {required this.title,
+      required this.imgUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
+      Key? key})
+      : super(key: key);
 
-  void selectMeal(){
-
+  String get complexityText{
+    switch (complexity){
+      case Complexity.challenging:
+        return 'Challenging';
+      case Complexity.hard:
+        return 'Hard';
+      case Complexity.simple:
+        return 'Simple';
+      default:
+        return 'Unknown';
+    }
   }
+
+  String get affordabilityText{
+    switch (affordability){
+      case Affordability.affordable:
+        return 'Affordable';
+      case Affordability.luxurious:
+        return 'Expensive';
+      case Affordability.pricey:
+        return 'Pricey';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  void selectMeal() {}
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: selectMeal,
       child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         elevation: 4,
         margin: const EdgeInsets.all(10),
-        child: Column(
-          children: <Widget> [
-            Stack(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
+        child: Column(children: <Widget>[
+          Stack(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(
+                  imgUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  color: Colors.black54,
+                  width: 300,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                    ),
+                    overflow: TextOverflow.fade,
+                    softWrap: true,
                   ),
-                  child: Image.network(imgUrl, height: 250, width: double.infinity, fit: BoxFit.cover,),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    const Icon(Icons.schedule),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text('$duration min'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    const Icon(Icons.work),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(complexityText),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    const Icon(Icons.attach_money),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(affordabilityText),
+                  ],
                 )
               ],
-            )
-          ]
-        ),
+            ),
+          )
+        ]),
       ),
     );
   }
