0706379db6903483b18eca86e12fbc5ee5a58a05
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-02-09 20:32:48 +0200

    Update 4: add smth news

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail-screen';
+
+  Widget buildSectionTitle(BuildContext context, String text){
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        'Ingredients',
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
   const MealDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == routeArgs);
     return Scaffold(
-      appBar: AppBar(title: Text(routeArgs),),
-      body: Center(
-        child: Text('This meal-$routeArgs!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(selectedMeal.title),
+        ),
+        body: Column(children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          buildSectionTitle(context, 'Ingredients'),
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            height: 150,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Text(selectedMeal.ingredients[index])),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          )
+        ]));
   }
 }
