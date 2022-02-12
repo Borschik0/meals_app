f47368bcac41b7c572cba885d00b7fa305e10efa
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-02-12 05:50:30 +0200

    Update 7

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,20 +11,39 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = const [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories'
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite'
+    }
+  ];
+  int _selectedPageIndex = 0;
+  void _selectedPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child: Scaffold(
-      appBar: AppBar(title: const Text('Meals'),
-      bottom: const TabBar(tabs: <Widget>[
-        Tab(icon: Icon(Icons.category), text: 'Category',),
-        Tab(icon: Icon(Icons.star), text: 'Favorites',),
-      ])),
-      body: const TabBarView(
-        children: <Widget>[
-          CategoriesScreen(),
-          FavoritesScreen()
+    return Scaffold(
+      appBar: AppBar(title:  Text(_pages[_selectedPageIndex]['title'] as String)),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        onTap: _selectedPage,
+        items: [
+          BottomNavigationBarItem(icon: const Icon(Icons.category), label:'Category', backgroundColor: Theme.of(context).primaryColor),
+          BottomNavigationBarItem(icon: const Icon(Icons.star), label: 'Favorites', backgroundColor: Theme.of(context).primaryColor)
         ],
       ),
-    ));
+    );
   }
 }
